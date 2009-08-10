0dd48a1522902efdce11d1e962ae84b459b12d97
jpablo <jpablo@a15ea993-2c5a-0410-b96a-e3977f56398a>
2009-08-10 05:00:46 +0000

    git-svn-id: http://superficie.svn.sourceforge.net/svnroot/superficie@40 a15ea993-2c5a-0410-b96a-e3977f56398a

--- Animation.py
+++ Animation.py
@@ -27,6 +27,29 @@ class Animation(QtCore.QTimeLine):
             connect(animations[i].pauseTimer,"timeout()",animations[i+1].start)
 
 
+class Timer(QtCore.QTimer):
+    "evalua automaticamente una funcion en los puntos del intervalo dado"
+    def __init__(self,func,rango):
+        QtCore.QTimer.__init__(self, None)
+        self.func = func
+        self.rango = rango
+        self.reset()
+        connect(self, "timeout()", self.update)
+
+    def update(self):
+        try:
+            self.intervalo.next()
+        except StopIteration, dir:
+            self.stop()
+            self.emit(QtCore.SIGNAL("finished()"))
+
+    def reset(self):
+        self.intervalo = GenIntervalo(self.rango,self. func)
+
+    def restart(self, msec):
+        self.reset()
+        self.start(msec)
+
 if __name__ == "__main__":
     import sys
     from PyQt4 import QtGui
