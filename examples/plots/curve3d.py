2b03853c18c594d51af6741af7ddc00c31251da7
juan pablo romero <jpablo.romero@gmail.com>
2013-03-15 01:22:48 -0600

    +comment

--- examples/plots/curve3d.py
+++ examples/plots/curve3d.py
@@ -20,6 +20,7 @@ def loxodrome(t):
     t = t * sigmoid(t)
     return r * cos(-t) / cosh(m * (-t - t0)), r * sin(-t) / cosh(m * (-t - t0)), r * tanh(m * (-t - t0))
 
+# max_distance and max_angle are hints for the refinement algorithm (optionals)
 curve = Curve3D(loxodrome, (-75, 60, 10), width=1, max_distance=.3, max_angle=.2)
 
 viewer.addChild(curve).viewAll().resize(400, 400)
